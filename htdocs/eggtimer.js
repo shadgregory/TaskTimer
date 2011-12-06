1e173ec9a977259275186bbde3a2b859adafff34
Shad Gregory <captshadg@gmail.com>
2011-12-05 23:48:39 -0600

    added ssl

--- htdocs/eggtimer.js
+++ htdocs/eggtimer.js
@@ -1,25 +1,54 @@
+function cmp_passwords () {
+    if ($('#new_password').val() == $('#new_password2').val()){
+	return true;
+    } else {
+	$('#message_div').html("The passwords do not match.");
+	return false;
+    }
+}
+
+function check_login () {
+    if ($('#password').val() == '') {
+	$('#message_div').html("Password is required.");
+	return false;
+    }
+    return true;
+}
+
 function add_task() {
     var d = new Date();
-    //var task_count = $("#tasks-list").children().length;
+//    $("#no-tasks").hide();
     var task_count = $('#tasks-table tr').length - 1;
     var task_row = $("<tr id='task_" + 
-	task_count + 
-	"'><td><input type='hidden' value='" + 
-	d.getTime() +
-	"' id='starttime_"+
-	task_count +
-	"'><input type='text' id='bug_num_"+ 
-	task_count + 
-	"'></td><td><input type='text' id='comment_" + 
-	task_count + 
-	"'></td><td colspan='2'><button onclick='cancel_task(" + 
-	task_count + 
-	")'>CANCEL</button><button onclick='end_task(" +
-	task_count + 
-	")'>END</button></td></tr>");
+		     task_count + 
+		     "'><td><input type='hidden' value='" + 
+		     d.getTime() +
+		     "' id='starttime_"+
+		     task_count +
+		     "'><input type='text' id='bug_num_"+ 
+		     task_count + 
+		     "'></td><td><input type='text' id='auto_cat" + 
+		     task_count + 
+		     "'></td><td><input type='text' id='comment_" + 
+		     task_count + 
+		     "'></td><td colspan='2'><button onclick='cancel_task(" + 
+		     task_count + 
+		     ")'>CANCEL</button><button onclick='end_task(" +
+		     task_count + 
+		     ")'>END</button></td></tr>");
+
 
     $("#tasks-table tr:last").after(task_row);
-    task_count = task_count + 1;
+
+    YUI().use('event', 'autocomplete', 'autocomplete-highlighters', function(Y) {
+	Y.Event.onAvailable('#auto_cat' + task_count, function(e) {
+	    Y.one('#auto_cat'+task_count).plug(Y.Plugin.AutoComplete, {
+		resultHighlighter: 'phraseMatch',
+		source: ['QA (R&D)','QA (Support)','R&D','R&D Planning','R&D Documentation','IT']
+	    });
+	});
+    });
+
 }
 
 function end_task(count) {
@@ -29,13 +58,16 @@ function end_task(count) {
 	alert("Either bug number or comment is required.");
 	return false;
     }
+    alert($("#auto_cat" + count).val());
+
     $.ajax({
 	url: "save-task",
 	context: document.body,
 	data: "bugnumber=" + $("#bug_num_" + count).val() +
-	      "&comment=" + $("#comment_" + count).val() +
-	      "&starttime=" + $("#starttime_" + count).val() +
-	      "&endtime=" + d.getTime(),
+	    "&comment=" + $("#comment_" + count).val() +
+	    "&category=" + encodeURIComponent($("#auto_cat" + count).val()) +
+	    "&starttime=" + $("#starttime_" + count).val() +
+	    "&endtime=" + d.getTime(),
 	success: function() {
 	    alert("task saved");
 	    $('#task_' + count).remove();
@@ -50,44 +82,69 @@ function cancel_task(count) {
 function init(Y) {
     var tabview = new Y.TabView({srcNode:'#timertab'});
     tabview.render();
-    YUI().use("datasource", "datasource-get", "datasource-io", "datasource-xmlschema", "datasource-textschema", "datatable-datasource", function(Y){
-	var cols = ["Bug Number", "Comment", "Hours"];
-	var dataSource = new Y.DataSource.IO({
-	    source:"/get-tasks?"
-	});
-	
-	dataSource.plug(Y.Plugin.DataSourceXMLSchema, {
-	    schema: {
-		resultListLocator: "task",
-		resultFields:[
-		    {key:"Bug Number", locator:"*[local-name()='bugnumber']"},
-		    {key:"Comment", locator:"*[local-name()='comment']"},
-		    {key:"Hours", locator:"*[local-name()='hours']"}
-		]
-	    }
-	});
+    YUI().use("datasource", 
+	      "datasource-get", 
+	      "datasource-io", 
+	      "datasource-xmlschema", 
+	      "datatable-sort", 
+	      "datatable-scroll", 
+	      "datatype-date",
+	      "autocomplete",
+	      "datatable-datasource", 
+	      function(Y){
+		  var formatDates = function (o){
+		      var dateObj = eval(o.value);
+		      return (dateObj.getMonth()+1) + "/" + dateObj.getDate() + "/" + dateObj.getFullYear();
+		  };
+		  var cols = [
+		      {key: "Bug Number", sortable: true},
+		      {key: "Category", sortable: true},
+		      {key: "Comment", sortable: true},
+		      {key: "Hours", sortable: true},
+		      {key: "End", formatter: formatDates, sortable: true}
+		  ];
+		  var dataSource = new Y.DataSource.IO({
+		      source:"/get-tasks?"
+		  });
 
-	var myCallback = {
-            success: function(e){
-		alert(e.response);
-            },
-            failure: function(e){
-                alert("Could not retrieve data: " + e.error.message);
-            }
-        };
+		  dataSource.plug(Y.Plugin.DataSourceXMLSchema, {
+		      schema: {
+			  resultListLocator: "task",
+			  resultFields:[
+			      {key:"Bug Number", locator:"*[local-name()='bugnumber']"},
+			      {key:"Category", locator:"*[local-name()='category']"},
+			      {key:"Comment", locator:"*[local-name()='comment']"},
+			      {key:"Hours", locator:"*[local-name()='hours']"},
+			      {key:"End", locator:"*[local-name()='enddate']"}
+			  ]
+		      }
+		  });
 
-	var table = new Y.DataTable.Base({
-	    columnset: cols,
-	    summary: "Tasks",
-	    caption: ""
-	}).render("#all-tasks");
+		  var myCallback = {
+		      success: function(e){
+			  alert(e.response);
+		      },
+		      failure: function(e){
+			  alert("Could not retrieve data: " + e.error.message);
+		      }
+		  };
 
-	table.plug(Y.Plugin.DataTableDataSource, {
-	    datasource: dataSource,
-	    initialRequest: ""
-	});
-	dataSource.after("response", function(){
-	    table.render("all-tasks");
-	});
-    });
+		  var table = new Y.DataTable.Base({
+		      columnset: cols,
+		      summary: "Tasks",
+		      caption: ""
+		  }).plug(Y.Plugin.DataTableSort).render("#all-tasks");
+
+		  table.plug(Y.Plugin.DataTableDataSource, {
+		      datasource: dataSource,
+		      initialRequest: ""
+		  });
+		  table.plug(Y.Plugin.DataTableScroll, {
+		      width: "500px",
+		      height: "600px"
+		  });
+		  dataSource.after("response", function(){
+		      table.render("all-tasks");
+		  });
+	      });
 }
--- main.rkt
+++ main.rkt
@@ -8,50 +8,125 @@
          web-server/http/redirect
          web-server/http/bindings
          racket/list
+	 racket/date
          file/md5
 	 xml
          web-server/servlet-env)
 (require (planet "main.rkt" ("jaymccarthy" "mongodb.plt" 1 11)))
 (require (planet "main.ss" ("dherman" "json.plt" 3 0)))
+;(require (file "../OAuth-2.0/authorization.rkt"))
 
-(define m (create-mongo))
-(define d (make-mongo-db m "eggtimer"))
+(define m (create-mongo))(define d (make-mongo-db m "eggtimer"))
 (current-mongo-db d)
 (define-mongo-struct task "task"
   ((username #:required)
    (bugnumber)
+   (category)
    (comment)
    (starttime #:required)
    (endtime #:required)))
 
+(define-mongo-struct
+  user "user"
+  ([username #:required]
+   [password #:required]))
+
 (define timer-page
   (lambda (req)
-    (response/xexpr
-    `(html
-      (head
-       (link ((type "text/css")(rel "stylesheet")(href "http://yui.yahooapis.com/3.4.1/build/cssfonts/fonts-min.css"))" ")
-       (script ((type "text/javascript")(src "eggtimer.js")) " ")
-       (script ((src "http://yui.yahooapis.com/3.4.1/build/yui/yui-min.js")(charset "utf-8"))" ")
-       (script ((type "text/javascript")(src "jquery-1.6.4.js")) " "))
-      (body ((class "yui3-skin-sam yui-skin-sam"))
-       (div ((id "timertab")(style "margin-left:20%;margin-right:20%;"))
-	    (ul
-	     (li
-	      (a ((href "#tasks-list")) "Create Tasks"))
-	     (li
-	      (a ((href "#datatable")) "Data")))
-	    (div
-	     (div ((id "tasks-table")(style "padding: 5px"))
-		  (a ((href "#")(onclick "add_task()")) "Add Task")
-		  (table ((id "tasks-table")(border "1"))
-			 (tr
-			  (th "Bug Number")
-			  (th "Notes")
-			  (th ""))))
-	     (div ((id "datatable")) 
-		  (div ((id "all-tasks"))))))
-       (script ((type "text/javascript"))
-	       "YUI({filter: 'raw'}).use('yui', 'tabview', function(Y){ init(Y)});"))))))
+    (define cookies (request-cookies req))
+    (define id-cookie
+      (findf (lambda (c)
+               (string=? "id" (client-cookie-name c)))
+             cookies))
+    (if id-cookie
+        (let ((username (client-cookie-value id-cookie)))
+	  (response/xexpr
+	   `(html
+	     (head
+	      (title ,(string-append "Task Timer - " username))
+	      (link ((type "text/css")(rel "stylesheet")(href "http://yui.yahooapis.com/3.4.1/build/cssfonts/fonts-min.css"))" ")
+	      (script ((type "text/javascript")(src "eggtimer.js")) " ")
+	      (script ((src "http://yui.yahooapis.com/3.4.1/build/yui/yui-min.js")(charset "utf-8"))" ")
+	      (script ((type "text/javascript")(src "jquery-1.6.4.js")) " "))
+	     (body ((class "yui3-skin-sam yui-skin-sam")(bgcolor "#e5e5e5"))
+		   (div ((id "timertab")(style "margin-left:25%;margin-right:25%;"))
+			(ul
+			 (li
+			  (a ((href "#tasks-list")) "Create Tasks"))
+			 (li
+			  (a ((href "#datatable")) "Data")))
+			(div
+			 (div ((id "tasks-table")(style "padding: 5px"))
+			      (a ((href "#")(onclick "add_task()")) "Add Task")
+			      (table ((id "tasks-table"))
+				     (tr
+				      (th ((style "min-width:100px")) "Bug Number")
+				      (th ((style "min-width:100px")) "Category")
+				      (th ((style "min-width:100px")) "Notes")
+				      (th ((colspan "2")(style "min-width:200px")) ""))))
+			      (div ((id "datatable")) 
+				   (div ((id "all-tasks")))))
+			(script ((type "text/javascript"))
+				"YUI({filter: 'raw'}).use('yui', 'tabview', function(Y){ init(Y)});"))))))
+	(redirect-to "https://shadgregory.net:8081"))))
+
+(define get-msg
+  (lambda (request)
+    (define bindings-lst (extract-bindings 'msg (request-bindings request)))
+    (cond ((empty? bindings-lst) " ")
+          ((null? bindings-lst) " ")
+          (else (car bindings-lst)))))
+
+(define current-user?
+  (lambda (username password)
+    (define current? #f)
+    (for/list ((u (mongo-dict-query "user" (hasheq))))
+      (cond
+        ((and (string=? username (user-username u))
+              (bytes=? (md5 password)(user-password u)))
+         (set! current? #t))
+        (else '())))
+    current?))
+
+(define new-user?
+  (lambda (username)
+    (define new? #t)
+    (for/list ((u (mongo-dict-query "user" (hasheq))))
+      (cond
+        ((string=? username (user-username u))
+         (set! new? #f))
+        (else '())))
+    new?))
+
+(define insert-new-user
+  (lambda (username password)
+    (cond 
+      ((new-user? username)
+       (make-user #:username username
+                  #:password (md5 password))
+       #t)
+      (else
+       #f))))
+
+(define user-formlet
+  (formlet
+   (table
+    (tr (td "Username:")
+        (td ,{input-string . => . username}))
+    (tr (td "Password:")
+        (td ,{(to-string (required (password-input))) . => . password})))
+   (list username password)))
+
+(define new-user-formlet
+  (formlet
+   (table
+    (tr (td "Username:")
+        (td ,{input-string . => . username}))
+    (tr (td "Password:")
+        (td ,{(to-string (required (password-input))) . => . new_password1}))
+    (tr (td "Confirm:")
+        (td ,{(to-string (required (password-input))) . => . new_password2})))
+   (list username new_password1 new_password2)))
 
 (define get-tasks
   (lambda (req)
@@ -62,35 +137,108 @@
     (response/xexpr
      `(tasks
        ,@(for/list ((t task-match))
+		   (let* ((enddate (seconds->date (round (/ (string->number (mongo-dict-ref t 'endtime)) 1000))))
+			  (year (date-year enddate))
+			  (day (date-day enddate))
+			  (month (date-month enddate)))
 		   `(task
-		     (hours ,(real->decimal-string (/ (/ (- (string->number (mongo-dict-ref t 'endtime))
-					       (string->number (mongo-dict-ref t 'starttime))) 1000) 60)))
+		     (hours ,(real->decimal-string (/ (/ (/ (- (string->number (mongo-dict-ref t 'endtime))
+					       (string->number (mongo-dict-ref t 'starttime))) 1000) 60) 60)))
 		     (endtime ,(mongo-dict-ref t 'endtime))
+		     (enddate ,(string-append "new Date(" (mongo-dict-ref t 'endtime) ")"))
 		     (starttime ,(mongo-dict-ref t 'starttime))
 		     (comment ,(mongo-dict-ref t 'comment))
-		     (bugnumber ,(mongo-dict-ref t 'bugnumber)))))
+		     (category ,(mongo-dict-ref t 'category))
+		     (bugnumber ,(mongo-dict-ref t 'bugnumber))))))
      #:mime-type #"application/xml"))))
+
+(define validate-user
+  (lambda (req)
+    (cond
+      ((current-user? (car (formlet-process user-formlet req))
+                      (second
+                       (formlet-process user-formlet req)))
+       (define id-cookie 
+         (make-cookie "id" 
+                      (car (formlet-process user-formlet req)) #:secure? #t))
+       (redirect-to "timer" #:headers (list (cookie->header id-cookie))))
+      (else
+       (redirect-to "https://shadgregory.net:8081?msg=baduser")))))
+
+(define validate-new-user
+  (lambda (req)
+    (let ((data-list (formlet-process new-user-formlet req)))
+      (cond
+        ((string=? (second data-list)(third data-list))
+         (cond
+           ((insert-new-user (car data-list) (second data-list))
+            (define id-cookie (make-cookie "id" (car data-list) #:secure? #t))
+            (redirect-to "timer" #:headers (list (cookie->header id-cookie))))
+           (else
+            (redirect-to "https://shadgregory.net:8081?msg=notnew"))))
+        (else
+         (redirect-to "https://shadgregory.net:8081?msg=nomatch"))))))
+
+(define logon-page
+  (lambda (req)
+    (define msg (get-msg req))
+    (response/xexpr
+     `(html
+       (head (title "Tommy Windich")
+             (script ((type "text/javascript")(src "jquery-1.6.4.js")) " ")
+             (script ((type "text/javascript") (src "yahoo-min.js")) " ")
+             (script ((type "text/javascript") (src "event-min.js")) " ")
+             (script ((type "text/javascript") (src "datasource-min.js")) " ")
+             (script ((type "text/javascript")(src "eggtimer.js"))" "))
+       (body ((bgcolor "#e5e5e5"))
+             (div ((id "center_content")
+                   (style "margin-left:auto;margin-right:auto;width:700px;"))
+                  (div ((style "border:1px solid black;background:#99CCFF;padding-top:5px;padding-left:5px;"))
+                       (div ((id "message_div") (style "color:red;")) 
+                            ,(cond
+                               ((string=? msg "notnew")
+                                "Please choose another user name.")
+                               ((string=? msg "nomatch")
+                                "Your passwords did not match.")
+                               ((string=? msg "baduser")
+                                "Login failed.")
+                               (else
+                                " ")))
+                       (form ((id "logon_form")
+                              (action "validate-user")
+                              (onsubmit "return check_login();"))
+                             ,@(formlet-display user-formlet)
+                             (br)
+                             (input ((type "submit")(name "login")(value "Login")))))
+                  (div ((style "border:1px solid black;background:#99CCFF;padding-top:5px;padding-left:5px;"))
+                       (form ((id "create_logon_form") 
+                              (action "validate-new-user")
+                              (onsubmit "return cmp_passwords();"))
+                             ,@(formlet-display new-user-formlet)
+                             (br)
+                             (input ((type "submit")(name "login")(value "Create Account")))
+                             ))))))))
     
 (define save-task
   (lambda (req)
+    (define cookies (request-cookies req))
+    (define id-cookie
+      (findf (lambda (c)
+	       (string=? "id" (client-cookie-name c)))
+    cookies))
     (define bindings (request-bindings req))
-    (define d (make-mongo-db (create-mongo) "eggtimer"))
-    (current-mongo-db d)
-    (define-mongo-struct task "task"
-      ((username #:required)
-       (bugnumber)
-       (comment)
-       (starttime #:required)
-       (endtime #:required)))
     (let*
 	((bugnumber (extract-binding/single 'bugnumber bindings))
 	 (comment (extract-binding/single 'comment bindings))
 	 (endtime (extract-binding/single 'endtime bindings))
+	 (category (extract-binding/single 'category bindings))
+	 (username (client-cookie-value id-cookie))
 	 (starttime (extract-binding/single 'starttime bindings)))
-	  
-      (make-task #:username "shad"
+      	  
+      (make-task #:username username
 		 #:bugnumber bugnumber
 		 #:comment comment
+		 #:category category
 		 #:endtime endtime
 		 #:starttime starttime))
     (response/xexpr
@@ -101,15 +249,22 @@
 
 (define-values (eggtimer-dispatch tracker-url)
   (dispatch-rules
+   (("") logon-page)
    (("save-task") save-task)
    (("get-tasks") get-tasks)
-   (("") timer-page)))
+   (("validate-new-user") validate-new-user)
+   (("validate-user") validate-user)
+   (("timer") timer-page)))
 
 (serve/servlet start
                #:launch-browser? #f
                #:quit? #f
+	       #:ssl? #t
                #:listen-ip #f
                #:port 8081
+               #:ssl-cert (build-path "/home/shad/mysrc/eggtimer" "server-cert.pem")
+               #:ssl-key (build-path "/home/shad/mysrc/eggtimer" "private-key.pem")
                #:servlet-regexp #rx""
-               #:extra-files-paths (list (build-path "/home/shad/mysrc/eggtimer/htdocs"))
+               #:extra-files-paths (list 
+				    (build-path "/home/shad/mysrc/eggtimer/htdocs"))
                #:servlet-path "/main.rkt")
