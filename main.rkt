7d4d9fef4bc89456a669d76e6d4f4d50d47dfa4c
Shad Gregory <captshadg@gmail.com>
2012-06-02 04:53:52 -0500

    more bug number

--- main.rkt
+++ main.rkt
@@ -348,8 +348,7 @@
                  (enddate ,(mongo-dict-ref t 'endtime))
                  (starttime ,(mongo-dict-ref t 'starttime))
                  (comment ,(mongo-dict-ref t 'comment))
-                 (category ,(mongo-dict-ref t 'category))
-                 (bugnumber ,(mongo-dict-ref t 'bugnumber))))))
+                 (category ,(mongo-dict-ref t 'category))))))
        #:mime-type #"application/xml"))))
 
 (define remove-doc
