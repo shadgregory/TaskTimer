46dd500a892092b5b28e62d85daa6d4d3aed4577
Shad Gregory <captshadg@gmail.com>
2012-09-13 12:40:29 -0500

    Updated for TranquilTrak

--- analytics.pl
+++ analytics.pl
@@ -15,26 +15,38 @@ $month_hash{'May'} = 5;
 $month_hash{'Jun'} = 6;
 $month_hash{'Jul'} = 7;
 $month_hash{'Aug'} = 8;
-$month_hash{'Sept'} = 9;
+$month_hash{'Sep'} = 9;
 $month_hash{'Oct'} = 10;
 $month_hash{'Nov'} = 11;
 $month_hash{'Dec'} = 12;
+my %robots_hsh = {};
 while (<FILE>) {
 	chomp;
-	next if ($_ =~ m/mint/);
-	next if ($_ =~ m/yui/);
-	next if ($_ =~ m/css/);
-	next if ($_ =~ m/png/);
-	next if ($_ =~ m/ttf/);
-	next if ($_ =~ m/favicon/);
-	next if ($_ =~ m/woff/);
-	next if ($_ =~ m/\.js/);
-	if (!$hsh{$ip_address}) {
-		$hsh{$ip_address} = {};
+	if ($_ !~ m/base\.css/) {
+		next;
 	}
+	#next if ($_ =~ m/mint/);
+	#next if ($_ =~ m/yui/);
+	#next if ($_ =~ m/\.css/);
+	#next if ($_ =~ m/png/);
+	#next if ($_ =~ m/ttf/);
+	#next if ($_ =~ m/favicon/);
+	#next if ($_ =~ m/woff/);
+	#next if ($_ =~ m/chili/);
+	#next if ($_ =~ m/\.js/);
+	next if ($_ =~ m/72\.177\.38\.135/); #home
+	next if ($_ =~ m/24\.113\.188\.6/); #rocklin CA
+	#if (!$hsh{$ip_address}) {
+	#	$hsh{$ip_address} = {};
+	#}
     my @line_array = split(/\s/, $_);
     my $ip_address = $line_array[0];
     my $date = $line_array[3];
+	if ($_ =~ m/robots/) {
+		$robots_hsh{$ip_address} = 1;
+		next;
+	}
+	next if ($robots_hsh{$ip_address});
 	$date =~ s/\[//g;
 	$date =~ s/\]//g;
 	@date_array = split('/', $date);
@@ -62,14 +74,15 @@ format ANALYTICS =
 .
 format ANALYTICS_TOP =
   @||||||||||||||||||||||||||||||||||||  Pg @<
-  "Visits to Tommy Windich",        $%
+  "Visits to TranquilTrak",        $%
 
   IP Address       City              State            Code  Visits Date
   ---------------  ----------------  ---------------  ----  ------ --------------
 .
-open(ANALYTICS, ">analytics.txt") || die "Can't create new file, error = $!\n";
+open(ANALYTICS, ">report") || die "Can't create new file, error = $!\n";
 
-foreach $value (sort {$hsh{$b}->{count} <=> $hsh{$a}->{count} } keys %hsh) {
+#foreach $value (sort {$hsh{$b}->{count} <=> $hsh{$a}->{count} } keys %hsh) {
+foreach $value (sort {$hsh{$b}->{date} <=> $hsh{$a}->{date} } keys %hsh) {
 		next if (!$value);
 	$gi = Geo::IP->open("/home/shad/mysrc/tasktimer/GeoLiteCity.dat", GEOIP_STANDARD);
 	$ip_address = $value;
